b4e0d56a4832e7f7049df8ad546cd7867421ee92
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-07-23 11:19:58 +0200

    Light app.ts Build script

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,3 @@
 import * as d3 from 'd3';
 
-d3.select('body')
-    .append('svg')
-    .attr('width', 800)
-    .attr('height', 800)
-    .append('circle')
-    .attr('cx', 250)
-    .attr('cy', 250)
-    .attr('r', 200)
-    .style('fill', 'steelblue');
+d3.select('body');
