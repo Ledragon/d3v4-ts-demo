09e511d346e723033b1dc894ac30e95ff3354125
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-07-23 19:51:44 +0200

    Cleand app prestart script

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,4 @@
 import * as d3 from 'd3';
 
-d3.select('body')
-    .append('svg')
-    .append('circle');
-    console.log('hello, parallel watch');
+d3.select('body');
+console.log('hello, parallel watch');
