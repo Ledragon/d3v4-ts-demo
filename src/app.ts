4a77365ca9697abd00fb42a1d323392fe26883d9
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-08-13 16:26:24 +0200

    Draw circle

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import * as d3 from 'd3';
 
-d3.select('body');
-console.log('hello, parallel watch');
+d3.select('body')
+    .append('svg')
+    .append('circle')
+    .attr('cx', 50)
+    .attr('cy', 50)
+    .attr('r', 30)
+    .style('fill', 'steelblue');
