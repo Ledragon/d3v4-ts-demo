752d44927de6929c5b2ed1d8074de299ce570aee
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-08-15 12:55:47 +0200

    Plot area

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import * as d3 from 'd3';
 
-d3.select('body')
+const width = 960;
+const height = 480;
+
+let svg = d3.select('body')
     .append('svg')
-    .append('circle')
-    .attr('cx', 50)
-    .attr('cy', 50)
-    .attr('r', 30)
-    .style('fill', 'steelblue');
+    .attr('width', width)
+    .attr('height', height);
+
+let plotMargins = {
+    top: 30,
+    bottom: 30,
+    left: 150,
+    right: 30
+};
+let plotGroup = svg.append('g')
+    .classed('plot', true)
+    .attr('transform', `translate(${plotMargins.left},${plotMargins.top})`);
+
+let plotWidth = width - plotMargins.left - plotMargins.right;
+let plotHeight = height - plotMargins.top - plotMargins.bottom;
